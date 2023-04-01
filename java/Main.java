d891ab22224fb1fad354d4e4e30382d2b4008e83
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-01 17:13:33 +0300

    add initialization block

--- java/Main.java
+++ java/Main.java
@@ -1,16 +1,8 @@
-import classes.BFG;
+import classes.Bullet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new BFG(10000, 300, "ExtraGun"));
-
-        //BFG, ammo: 5000, damage: 200
-        System.out.println(new BFG(5000, 200));
-
-        //BFG, ammo: 2000, damage: 50
-        System.out.println(new BFG(2000));
-
-        //BFG, ammo: 1000, damage: 50
-        System.out.println(new BFG());
+        new Bullet();
+        new Bullet();
     }
 }
