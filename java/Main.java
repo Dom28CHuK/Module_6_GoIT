50381949627cb969dab88aadfda6793c2490e242
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-05 07:33:36 +0300

    add new classes Menu

--- java/Main.java
+++ java/Main.java
@@ -1,39 +1,16 @@
-import classes.Level;
+import classes.GameMenu;
 
 public class Main {
     public static void main(String[] args) {
-//        //Quarke level, name is Test
-//        System.out.println(new Level("Test"));
+        GameMenu menu = new GameMenu();
 
-//        Level.Point p1 = new Level.Point(3, 5);
-//        Level.Point p2 = new Level.Point(10, 100);
-//        Level.Point p3 = new Level.Point(50, 40);
-//
-//        Level.Point[] points = {p1, p2, p3};
-//
-//        //Quarke level, name is Test, point count is 3
-//        System.out.println(new Level("Test", points));
+        //options
+        menu.act(new GameMenu.Options());
 
-//        Level.Point p1 = new Level.Point(3, 5);
-//        Level.Point p2 = new Level.Point(10, 100);
-//        Level.Point p3 = new Level.Point(50, 40);
-//
-//        Level.Point[] points = {p1, p2, p3};
-//
-//        Level.LevelInfo info = new Level.LevelInfo("Quarke Intro", "Easy");
-//
-//        //Quarke level, name is Quarke Intro, difficulty is Easy, point count is 3
-//        System.out.println(new Level(info, points));
+        //start
+        menu.act(new GameMenu.Start());
 
-        Level.Point p1 = new Level.Point(3, 5);
-        Level.Point p2 = new Level.Point(10, 100);
-        Level.Point p3 = new Level.Point(50, 40);
-
-        Level.Point[] points = {p1, p2, p3};
-
-        Level.LevelInfo info = new Level.LevelInfo("Quarke Intro", "Easy");
-
-        //3015
-        System.out.println(new Level(info, points).calculateLevelHash());
+        //exit
+        menu.act(new GameMenu.Exit());
     }
 }
