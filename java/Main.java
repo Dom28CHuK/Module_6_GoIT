c47c855c44cb1fd775687554475dfeb080485dfc
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-05 08:39:57 +0300

    update

--- java/Main.java
+++ java/Main.java
@@ -1,11 +1,22 @@
+import classes.ArrayWorker;
 import classes.AvgDamageCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        //5
-        System.out.println(new AvgDamageCalculator().calculateAvg(new int[] {2, 4, 6, 8}));
+//        //5
+//        System.out.println(new AvgDamageCalculator().calculateAvg(new int[] {2, 4, 6, 8}));
+//
+//        //0
+//        System.out.println(new AvgDamageCalculator().calculateAvg(new int[0]));
 
-        //0
-        System.out.println(new AvgDamageCalculator().calculateAvg(new int[0]));
+        int[] array = {1, 2, 3};
+
+        //value is 1
+        //index is 0
+        new ArrayWorker().printElement(array, 0);
+
+        //wrong index
+        //index is 10
+        new ArrayWorker().printElement(array, 10);
     }
 }
