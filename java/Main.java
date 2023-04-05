b05c7cb963908e39cb2cbbfd3469154856ef0e2b
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-05 07:53:44 +0300

    up

--- java/Main.java
+++ java/Main.java
@@ -3,23 +3,26 @@ import classes.LevelLoader;
 
 public class Main {
     public static void main(String[] args) {
-        GameMenu menu = new GameMenu();
-
-        //options
-        menu.act(new GameMenu.Options());
-
-        //start
-        menu.act(new GameMenu.Start());
-
-        //exit
-        menu.act(new GameMenu.Exit());
-
-        LevelLoader levelLoader = new LevelLoader();
+//        GameMenu menu = new GameMenu();
+//
+//        //options
+//        menu.act(new GameMenu.Options());
+//
+//        //start
+//        menu.act(new GameMenu.Start());
+//
+//        //exit
+//        menu.act(new GameMenu.Exit());
+//
+//        LevelLoader levelLoader = new LevelLoader();
+//
+//        //Loading level Startlevel ...
+//        System.out.println(levelLoader.load("StartLevel"));
+//
+//        System.out.println(LevelLoader.getInstance().load("StartLevel"));
+//        System.out.println(LevelLoader.getInstance() == LevelLoader.getInstance());
 
         //Loading level Startlevel ...
-        System.out.println(levelLoader.load("StartLevel"));
-
         System.out.println(LevelLoader.getInstance().load("StartLevel"));
-        System.out.println(LevelLoader.getInstance() == LevelLoader.getInstance());
     }
 }
